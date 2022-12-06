a0a9b69ed4ec89cf05ba652ff0e74ab5f3926aae
danyzhr <danyzhr@live.fr>
2022-12-07 00:33:17 +0100

    :construction: update to avoid errors

--- brinder-match/src/main/java/org/gso/brinder/match/service/UserService.java
+++ brinder-match/src/main/java/org/gso/brinder/match/service/UserService.java
@@ -1,8 +1,10 @@
 package org.gso.brinder.match.service;
 
 import lombok.RequiredArgsConstructor;
+import org.gso.brinder.match.model.Coordonnee;
 import org.gso.brinder.match.model.User;
 import org.gso.brinder.match.repository.UserRepository;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -14,17 +16,20 @@ public class UserService {
     private final UserRepository userRepository;
 
 
-    public User getUser(JWTAuthenticationToken token) {
-        userRepository.findById(token.getId());
+    public User getUser(JwtAuthenticationToken token) {
+        return userRepository.findById(token.getTokenAttributes().get("").toString()).orElse(null);
     }
 
     // METTRE A JOUR LES COORDONNEES
-    public void updateUser(JWTAuthenticationToken token) {
+    public void updateUserLocation(JwtAuthenticationToken token) {
 
     }
-
-    public List<User> searchSurroundingUsers(JWTAuthenticationToken token) {
-
+    // RECUPERER LES USERS 100M AUX ALENTOURS
+    public List<User> searchSurroundingUsers(JwtAuthenticationToken token) {
+        List<User> surrounding_users = userRepository.findAll();
+        return  surrounding_users;
     }
+
+    public List<User> getAllUsers() { return userRepository.findAll(); }
 
 }
