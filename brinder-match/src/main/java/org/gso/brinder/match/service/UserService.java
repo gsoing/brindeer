981357473a4ff1e149dd5b45b38ef2525f4c85d4
Dany ZAHER <danyzhr@live.fr>
2022-12-08 00:04:06 +0100

    use google map api to convert address to point

--- brinder-match/src/main/java/org/gso/brinder/match/service/UserService.java
+++ brinder-match/src/main/java/org/gso/brinder/match/service/UserService.java
@@ -1,5 +1,6 @@
 package org.gso.brinder.match.service;
 
+import com.nimbusds.jose.shaded.json.JSONObject;
 import lombok.RequiredArgsConstructor;
 import org.gso.brinder.match.model.Coordonnee;
 import org.gso.brinder.match.model.User;
@@ -8,15 +9,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.GeoResult;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 import java.awt.*;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@RequestMapping("")
 public class UserService {
 
     private final UserRepository userRepository;
@@ -27,28 +32,45 @@ public class UserService {
     }
 
     // METTRE A JOUR LES COORDONNEES
-    public void updateUserLocation(JwtAuthenticationToken token,@RequestBody Coordonnee coordonnee) {
+    public void updateUserLocation(JwtAuthenticationToken token) {
+//        double[] coordinate = addressToCoordinate(token);
+//        userRepository.save(new User(token.getTokenAttributes().get("sub").toString(),
+//                token.getTokenAttributes().get("given_name").toString(),
+//                token.getTokenAttributes().get("family_name").toString(),
+//                token.getTokenAttributes().get("email").toString(),
+//                (Integer) token.getTokenAttributes().get("age"),
+//                coordinate[0],
+//                coordinate[1]));
+    }
 
-        userRepository.save(new User(token.getTokenAttributes().get("sub").toString(),
-                token.getTokenAttributes().get("given_name").toString(),
-                token.getTokenAttributes().get("family_name").toString(),
-                token.getTokenAttributes().get("email").toString(),
-                (Integer) token.getTokenAttributes().get("age"),
-                (double) coordonnee.getLocation()[0],
-                (double) coordonnee.getLocation()[1]));
+    @GetMapping("/address")
+    private void addressToCoordinate(JwtAuthenticationToken token) {
+//        String address = (String) token.getTokenAttributes().get("address");
+        String address = "31 Rue Camille Mouquet Charenton-Le-Pont France";
+        String url = "https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyC6M0Wt1zio5q8b5ZfQYiNjZU7OVE4s72s&address="
+                + java.net.URLEncoder.encode(address, StandardCharsets.UTF_8).replace("+", "%20");
+        logger.info(url);
+
+        RestTemplate restTemplate = new RestTemplate();
+        JSONObject result = restTemplate.getForObject(url, JSONObject.class);
+//        double latitude = result.getJSONObject("results").getJSONObject("0").getJSONObject("geometry").getJSONObject("location").get("lat");
+//        double longitude = result.getJSONObject("results").getJSONObject("0").getJSONObject("geometry").getJSONObject("location").get("lng")
+        double latitude = result.get("lat"); double longitude = result.get("lng");
+        logger.info("latitude = " + latitude + ", longitude = " + longitude);
     }
+
     // RECUPERER LES USERS 100M AUX ALENTOURS
-    public List<User> searchSurroundingUsers(JwtAuthenticationToken token,@RequestBody Coordonnee coordonnee) {
-        List<GeoResult<User>> results = userRepository.findByLocationNear(
-                new Point(coordonnee.getLocation()[0],coordonnee.getLocation()[1]), new Distance(100)
-        ).getContent();
-        List<User> returned_result = null;
-        for (GeoResult<User> result : results) {
-            if (result.getContent().getId() != token.getTokenAttributes().get("sub"))
-                returned_result.add(result.getContent());
-        }
-        return returned_result;
-    }
+//    public List<User> searchSurroundingUsers(JwtAuthenticationToken token) {
+//        List<GeoResult<User>> results = userRepository.findByLocationNear(
+//                new Point(coordonnee.getLocation()[0],coordonnee.getLocation()[1]), new Distance(100)
+//        ).getContent();
+//        List<User> returned_result = null;
+//        for (GeoResult<User> result : results) {
+//            if (result.getContent().getId() != token.getTokenAttributes().get("sub"))
+//                returned_result.add(result.getContent());
+//        }
+//        return returned_result;
+//    }
 
     public List<User> getAllUsers() { return userRepository.findAll(); }
 
