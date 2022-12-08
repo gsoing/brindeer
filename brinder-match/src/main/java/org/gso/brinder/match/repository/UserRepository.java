d66f81a3962b4e671393d8755252565b8171c66e
danyzhr <danyzhr@live.fr>
2022-12-08 20:34:00 +0100

    use specific Point class

--- brinder-match/src/main/java/org/gso/brinder/match/repository/UserRepository.java
+++ brinder-match/src/main/java/org/gso/brinder/match/repository/UserRepository.java
@@ -4,12 +4,14 @@ import org.gso.brinder.match.model.Coordonnee;
 import org.gso.brinder.match.model.User;
 import org.springframework.data.geo.Distance;
 import org.springframework.data.geo.GeoResults;
+import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import java.awt.*;
+import java.awt.geom.Point2D;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-    GeoResults<User> findByLocationNear(Coordonnee location, Distance distance);
+    GeoResults<User> findByLocationNear(Point location, Distance distance);
 }
