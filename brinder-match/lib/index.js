b7e9633d684e1b67657e836927805d108031e5b9
Nabil B <nabilbataille@hotmail.com>
2024-02-17 00:49:29 +0100

    Update (api-manger) : kong & conf

--- brinder-match/lib/index.js
+++ brinder-match/lib/index.js
@@ -6,8 +6,8 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.use('/api/v1/users', usersRoutes);
-app.use('/api/v1/profiles', profilesRoutes);
+app.use('/match/v1/users', usersRoutes);
+app.use('/match/v1/profiles', profilesRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server started on port : ${PORT}`);
