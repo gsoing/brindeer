bed385a0c1b5d7cd04fc63a412461a38a385ee77
ThibaultNG <byosoris@gmail.com>
2024-02-16 23:44:40 +0100

    Match find profiles within 100m

--- brinder-match-api/src/main/java/org/gso/brinder/match/service/MatchService.java
+++ brinder-match-api/src/main/java/org/gso/brinder/match/service/MatchService.java
@@ -9,7 +9,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Service;
 import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.data.geo.Metrics;
+import org.springframework.data.geo.Distance;
 import java.util.Optional;
+import java.util.List;
 
 import lombok.RequiredArgsConstructor;
 
@@ -30,8 +33,12 @@ public class MatchService {
         return matchRepository.findById(profileId).orElseThrow(() -> NotFoundException.DEFAULT);
     }
 
-    public Optional<ProfileModel> findByEmail(String email){
+    public Optional<ProfileModel> findByEmail(String email) {
         return matchRepository.findByMail(email);
     }
 
+    public List<ProfileModel> findProfilesAround100m(GeoJsonPoint location) {
+        return matchRepository.findProfilesNearLocation(location.getX(), location.getY(),
+                new Distance(0.1, Metrics.KILOMETERS));
+    }
 }
