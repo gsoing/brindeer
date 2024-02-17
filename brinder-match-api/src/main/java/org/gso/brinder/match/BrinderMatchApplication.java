13e0960bdbb35e56d61f735f7f0ea8d144e86cbe
YannisFummi <yfummi91@gmail.com>
2024-02-17 19:57:56 +0100

    L'ANNOTATION QUI CLUTCH

--- brinder-match-api/src/main/java/org/gso/brinder/match/BrinderMatchApplication.java
+++ brinder-match-api/src/main/java/org/gso/brinder/match/BrinderMatchApplication.java
@@ -2,8 +2,10 @@ package org.gso.brinder.match;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class BrinderMatchApplication {
 
 	public static void main(String[] args) {
