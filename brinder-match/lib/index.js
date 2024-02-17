f9c499afa1c7863905de56eb2243313b9d21f36c
Clement MOULY <mouly.cl@gmail.com>
2024-02-17 10:29:16 +0100

    update routes to resolve conflicts

--- brinder-match/lib/index.js
+++ brinder-match/lib/index.js
@@ -6,8 +6,8 @@ const PORT = 3000;
 
 app.use(express.json());
 
-app.use('/api/v1/users', usersRoutes);
-app.use('/api/v1/profiles', profilesRoutes);
+app.use('/api/v1/matches/users', usersRoutes);
+app.use('/api/v1/matches/profiles', profilesRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server started on port : ${PORT}`);
