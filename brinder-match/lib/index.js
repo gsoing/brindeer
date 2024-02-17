d9ceace5458706c4777a2f04a64a9a1550e3f558
Nabil B <nabilbataille@hotmail.com>
2024-02-17 15:11:06 +0100

    Update (match) : swagger issue & log

--- brinder-match/lib/index.js
+++ brinder-match/lib/index.js
@@ -6,6 +6,12 @@ const PORT = 3000;
 
 app.use(express.json());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.use('/api/v1/matches/users', usersRoutes);
 app.use('/api/v1/matches/profiles', profilesRoutes);
 
--- brinder-match/lib/routes/users.js
+++ brinder-match/lib/routes/users.js
@@ -7,6 +7,7 @@ const User = require('../models/users');
 router.get('/', async (req, res) => {
     try {
         const users = await getUsers();
+        console.log( 'Get /users')
         res.status(200).json(users);
     } catch (error) {
         console.error('Error getting users:', error);
@@ -25,6 +26,7 @@ router.post('/', async (req, res) => {
         const newUser = new User(name, coordinates);
         await setUser(newUser);
 
+        console.log( 'Post /users')
         res.status(201).json({ message: "User created.", user: newUser });
     } catch (error) {
         console.error('Error creating user:', error);
